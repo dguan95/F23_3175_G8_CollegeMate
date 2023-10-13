1276647f73fc73fdb13ccfefdadcb4099ecdcef7
diana <dianagh23@outlook.com>
2023-10-12 19:11:58 -0700

    Creating project and sharing it on GitHub

--- app/src/main/java/com/example/collegemate/MainActivity.java
+++ app/src/main/java/com/example/collegemate/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Hello, this is a test - Diana
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
