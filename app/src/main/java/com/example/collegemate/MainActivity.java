47e7bf60673479ad3c7e3a7bea83cce9d76c6426
DianaG <89602697+dguan95@users.noreply.github.com>
2023-10-12 19:13:57 -0700

    Testing from GitHub - Diana

--- app/src/main/java/com/example/collegemate/MainActivity.java
+++ app/src/main/java/com/example/collegemate/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Hello, this is a test - Diana
+    //Hello, this is test again, editing on GitHub to see changes in my PC - Diana
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
