bf418a20ed49f16957a6b7ce7ea1aa1f08a5a338
OnlyRacoon <pavelant1338@gmail.com>
2023-11-04 14:00:30 -0700

    Login & Profile page commit

---
+++ app/src/main/java/com/example/collegemate/ProfilePage.java
@@ -0,0 +1,42 @@
+package com.example.collegemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfilePage extends AppCompatActivity {
+    TextView txtViewFName;
+    TextView TxtViewLName;
+    TextView TxtViewMajor;
+    TextView TxtViewDOB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+
+        txtViewFName = findViewById(R.id.txtViewFName);
+        TxtViewLName = findViewById(R.id.txtViewLName);
+        TxtViewDOB = findViewById(R.id.txtViewBirth);
+        TxtViewMajor = findViewById(R.id.txtViewMajor);
+
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+        String fName = bundle.getString("FNAME");
+        String lName = bundle.getString("LNAME");
+        String major = bundle.getString("MAJOR");
+        String day = bundle.getString("DAY");
+        String month = bundle.getString("MONTH");
+        String year = bundle.getString("YEAR");
+
+        txtViewFName.setText(fName);
+        TxtViewLName.setText(lName);
+        TxtViewMajor.setText(major);
+        TxtViewDOB.setText(day+" of " + month +", "+  year);
+
+
+
+    }
+}
