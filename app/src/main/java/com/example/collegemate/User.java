a9d1a6afeee51673c37081178f81da61907d8632
OnlyRacoon <pavelant1338@gmail.com>
2023-11-15 20:56:47 -0800

    db update commit

--- app/src/main/java/com/example/collegemate/User.java
+++ app/src/main/java/com/example/collegemate/User.java
@@ -1,10 +1,36 @@
 package com.example.collegemate;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     private long id;
     private String email;
     private String password;
+
+    private int birthYear;
+    private int birthMonth;
+    private int birthDate;
+
+    private String firstName;
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    private String lastName;
+
+    private String major;
+
+    private byte[] image;
+
+    public User(String email) {
+        this.email = email;
+    }
 
     public User() {
     }
@@ -14,6 +40,66 @@ public class User {
         this.password = password;
     }
 
+    public User(long id, String email, String password, int birthYear, int birthMonth, int birthDate, String firstName, String lastName, String major, byte[] image) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.birthYear = birthYear;
+        this.birthMonth = birthMonth;
+        this.birthDate = birthDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+        this.image = image;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(int birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
 
     public long getId() {
         return id;
