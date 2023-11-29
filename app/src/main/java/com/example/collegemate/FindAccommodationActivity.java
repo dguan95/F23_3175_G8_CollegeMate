916a88a618df2eb44d4fa7323a1506ad24536aed
diana <dianagh23@outlook.com>
2023-11-29 01:37:32 -0800

    Back btn for HousingTips and findAccommodation

--- app/src/main/java/com/example/collegemate/FindAccommodationActivity.java
+++ app/src/main/java/com/example/collegemate/FindAccommodationActivity.java
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -18,6 +21,7 @@ import java.util.List;
 public class FindAccommodationActivity extends AppCompatActivity {
 
     List<FloorPlans> FloorPlanList = new ArrayList<>();
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,14 @@ public class FindAccommodationActivity extends AppCompatActivity {
         AddFloorPlans();
 
         RecyclerView recyclerViewFloorPlans = findViewById(R.id.recyclerViewFloorPlans);
-
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setBackgroundColor(Color.BLACK);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
         //need to set this with onClickListener
         ImageView imgViewFloorPlans = findViewById(R.id.imgViewAccommodation);
 
