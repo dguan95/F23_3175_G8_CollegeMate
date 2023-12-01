324ef61c87a1bfef1b2cec36542a812b4aa53741
diana <dianagh23@outlook.com>
2023-11-30 22:46:10 -0800

    payment activity with external library

---
+++ app/src/main/java/com/example/collegemate/PaymentDetailsActivity.java
@@ -0,0 +1,72 @@
+package com.example.collegemate;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.braintreepayments.cardform.view.CardForm;
+
+public class PaymentDetailsActivity extends AppCompatActivity {
+
+    CardForm cardForm;
+    Button btnPayment;
+    AlertDialog.Builder alertBuilder;
+    Button btnCancelPayment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_details);
+
+        cardForm = findViewById(R.id.cardForm);
+        btnPayment = findViewById(R.id.btnProcessPayment);
+        btnCancelPayment = findViewById(R.id.btnCancelPayment);
+
+        cardForm.cardRequired(true);
+        cardForm.expirationRequired(true);
+        cardForm.cvvRequired(true);
+        cardForm.postalCodeRequired(true);
+        cardForm.cardholderName(CardForm.FIELD_REQUIRED);
+        cardForm.mobileNumberRequired(true);
+        cardForm.mobileNumberExplanation("Confirmation SMS will be sent once payment is successful");
+        cardForm.actionLabel("Make Payment");
+        cardForm.setup(PaymentDetailsActivity.this);
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+
+        btnPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cardForm.isValid()) {
+                    alertBuilder = new AlertDialog.Builder(PaymentDetailsActivity.this);
+                    alertBuilder.setTitle("Confirm details to proceed");
+                    alertBuilder.setMessage("Card holder: " + cardForm.getCardholderName() + "\n");
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(PaymentDetailsActivity.this, "Transaction Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+                } else {
+                    Toast.makeText(PaymentDetailsActivity.this, "Please enter the field", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
